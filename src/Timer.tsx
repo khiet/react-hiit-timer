053a1c6fc5857500051f517204eaf2bb19c549a3
Khiet Le <khiet@outlook.com>
2022-02-27 13:53:52 +0000

    yarn add use-sound

--- src/Timer.tsx
+++ src/Timer.tsx
@@ -1,11 +1,17 @@
 import { useState, useEffect } from 'react';
+import useSound from 'use-sound';
+
+import countDownSound from './assets/sounds/boop.mp3';
+import countDownFinishSound from './assets/sounds/pop-up-off.mp3';
 
 export const Timer = ({ interval, onTick, onFinish }: {
   interval: number,
   onTick: () => void,
   onFinish: () => void
 }) => {
-  const [time, setTime] = useState(interval);
+  const [time, setTime] = useState<number>(interval);
+  const [playCowntDown] = useSound(countDownSound);
+  const [playCowntDownFinish] = useSound(countDownFinishSound);
 
   useEffect(() => {
     const id = setInterval(() => {
@@ -19,8 +25,14 @@ export const Timer = ({ interval, onTick, onFinish }: {
 
   useEffect(() => {
     if (time === 0) {
+      playCowntDownFinish();
+
       onFinish();
     } else {
+      if (time <= 5) {
+        playCowntDown();
+      }
+
       onTick();
     }
   }, [time]);
